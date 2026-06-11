fb202a32f97b8de44cce03b5a06ffd2891530eac
taka03101111 <241205188@ccmailg.meijo-u.ac.jp>
2026-06-11 13:40:44 +0900

    Update image source path in Contact component

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -125,7 +125,7 @@ function Contact() {
            id="contact-visual"
            className="contact-visual reveal"
            data-delay="2"
-           src="../asahi.jpg"
+           src="/asahi.jpg"
         />
         </div>
       </div>
